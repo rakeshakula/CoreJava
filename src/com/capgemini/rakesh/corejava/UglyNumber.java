31b26e5311bfec9bbc5dd515dd06d1f547c9d424
Rakesh Akula <rakesh.akula@ymail.com>
2020-05-25 08:53:44 +0530

    Updated Ugly Number Program

--- src/com/capgemini/rakesh/corejava/UglyNumber.java
+++ src/com/capgemini/rakesh/corejava/UglyNumber.java
@@ -4,16 +4,30 @@ package com.capgemini.rakesh.corejava;
 import java.util.Scanner;
 
 public class UglyNumber {
-	public static void main(String args[])
-	{
-	Scanner sc = new Scanner(System.in);
-	int number;
-	System.out.println("Enter a number");
-	number=sc.nextInt();
-	if( number==1 || number%2==0 || number%3==0 || number%5==0 )
-		System.out.println(number+" is a ugly number");
-	else
-		System.out.println(number+" is not a ugly number");
+	public static void main(String args[]) {
+		System.out.println("Enter a number : ");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if (n <= 0) {
+			System.out.print("Input a correct number.");
+		}
+		int x = 0;
+		while (n != 1) {
+			if (n % 5 == 0) {
+				n /= 5;
+			} else if (n % 3 == 0) {
+				n /= 3;
+			} else if (n % 2 == 0) {
+				n /= 2;
+			} else {
+				System.out.print("It is not an ugly number.");
+				x = 1;
+				break;
+			}
+		}
+		if (x == 0)
+			System.out.print("It is an ugly number.");
+		System.out.print("\n");
 	}
 
 }
